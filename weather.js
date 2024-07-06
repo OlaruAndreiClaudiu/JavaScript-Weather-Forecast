7226472c7cd04de65b03f68fccd253ad5ff79f6e
Olaru Andrei Claudiu <olaru.andrei.claudiu@gmail.com>
2024-07-07 01:51:00 +0300

    Commit 5

--- weather.js
+++ weather.js
@@ -8,22 +8,19 @@ const createWeatherCard = (cityName, weatherItem, index) => {
     if(index === 0) {
         return `<div class="details">
                 <h2>${cityName} (${weatherItem.dt_txt.split(" ")[0]})</h2>
+                <img src="http://openweathermap.org/img/wn/${weatherItem.weather[0].icon}01d@2x.png" alt="weather-icon">
                 <h4>Temperature: ${(weatherItem.main.temp - 273.15).toFixed(2)}℃</h4>
                 <h4>Wind: ${weatherItem.wind.speed} M/S</h4>
                 <h4>Humidity: ${weatherItem.main.humidity}%</h4>                 
-                </div>
-                <div class="icon">
-                <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@4x.png" alt="weather-icon">
-                <h4>${weatherItem.weather[0].description}</h4>
-                </div>`;        
+                </div>`;       
     } else {
         return `<li class="card">
-            <h3>(${weatherItem.dt_txt.split(" ")[0]})</h3>
-            <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@4x.png" alt="weather-icon">
-            <h4>Temp: ${(weatherItem.main.temp - 273.15).toFixed(2)}℃</h4>
-            <h4>Wind: ${weatherItem.wind.speed} M/S</h4>
-            <h4>Humidity: ${weatherItem.main.humidity}%</h4>  
-            </li>`;
+                <h3>(${weatherItem.dt_txt.split(" ")[0]})</h3>
+                <img src="http://openweathermap.org/img/wn/${weatherItem.weather[0].icon}01d@4x.png" alt="weather-icon">
+                <h4>Temp: ${(weatherItem.main.temp - 273.15).toFixed(2)}℃</h4>
+                <h4>Wind: ${weatherItem.wind.speed} M/S</h4>
+                <h4>Humidity: ${weatherItem.main.humidity}%</h4>  
+                </li>`;
     }
 }
 const getWeatherDetails = (cityName, latitude, longitude) => {
